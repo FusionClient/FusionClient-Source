dee36deece53c152954d7cc77d875314b3e0e3c4
Fusion Client <90038205+Casesos@users.noreply.github.com>
2022-01-30 21:23:47 +1000

    LocalLootTrackerPlugin

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/localloottracker/LocalLootTrackerConfig.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 JumpIfZero <https://github.com/JumpIfZero>
+ */
+package net.runelite.client.plugins.localloottracker;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup(LocalLootTrackerPlugin.CONFIG_GROUP)
+public interface LocalLootTrackerConfig extends Config
+{
+    @ConfigItem(
+            keyName = "separateByRsn",
+            name = "Separate by RSN",
+            description = "Separate loot records by RSN instead of collection",
+            position = 0
+    )
+    default boolean separateByRsn() { return false; }
+}
