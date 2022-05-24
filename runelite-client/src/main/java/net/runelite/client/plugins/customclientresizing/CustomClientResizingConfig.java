d9aca838f67cfcb865048bb07d2c845a5befa4cc
Fusion Plugins <fusionplugins@gmail.com>
2022-05-24 12:30:33 +1000

    WIP

--- runelite-client/src/main/java/net/runelite/client/plugins/customclientresizing/CustomClientResizingConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/customclientresizing/CustomClientResizingConfig.java
@@ -6,4 +6,13 @@ import net.runelite.client.config.ConfigGroup;
 @ConfigGroup(CustomClientResizingPlugin.CONFIG_GROUP)
 public interface CustomClientResizingConfig extends Config
 {
+  /* WIP
+   * @ConfigItem(
+            keyName = "resizeToggleKey",
+            name = "Resize Toggle Key",
+            description = "The key that will toggle the resize currently selected (accepts modifiers)",
+            position = 1
+    )
+    default Keybind resizeToggleKey() { return Keybind.NOT_SET; }
+*/
 }
